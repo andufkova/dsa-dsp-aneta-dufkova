302a2aa6243fd987af996c365aa7772d1c1c95f5
Aneta Dufková <andufkova@gmail.com>
2021-06-17 18:54:01 +0200

    deleted unnecessary files from previous version

--- notebooks/model_evaluation.py
+++
@@ -1,6 +0,0 @@
-from sklearn.metrics import mean_squared_log_error
-import numpy as np
-
-def compute_rmsle(y_test: np.ndarray, y_pred: np.ndarray, precision: int = 2) -> float:
-    rmsle = np.sqrt(mean_squared_log_error(y_test, y_pred))
-    return round(rmsle, precision)
